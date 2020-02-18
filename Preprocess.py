4446499123726c7f9369fca4f72717cd52bb9ad4
Shreya98-code <agrawal.shreya@columbia.edu>
2020-02-17 19:02:45 -0500

    Initial commit

--- Preprocess.py
+++ Preprocess.py
@@ -1,113 +1,132 @@
+"""
+Assignment_3 Library for Tweet Preprocessing
+"""
 import re
-import numpy as np
 from nltoolkit import TweetTokenizer
+import numpy as np
 from nltk.corpus import stopwords 
 from nltk.tokenize import word_tokenize 
 
-class Preprocess:
+class Preprocess_tweet:
+
+	import numpy as np
+	"""
+	Class to perform preprocessing and generate padded embeddings of tweets
+	"""
 
 	def __init__(self, max_length_tweet =50 ,max_length_dictionary = 10000):
+
+		"""
+        Performing class initialization
+        :params max_length_tweet:
+        :params max_length_dictionary:
+        :params embeddings_dict:
+
+        """
+
 		self.max_length_tweet = max_length_tweet
 		self.max_length_dictionary = max_length_dictionary
+		self.embeddings_dict = {}
 
-	#Loading the dictionary
-	glov_files = ['glove_25d_1.txt', 'glove_25d_2.txt', 'glove_25d_3.txt']
+		#Loading the dictionary
+		glove_files = ['glove_25d_1.txt','glove_25d_2.txt','glove_25d_3.txt']
 
-        c = 0
+		j = 0
 
-        self.embeddings_dict = {}
-
-        for doc in glove_files:
-            if c >= max_length_dictionary:
-                break
-            with open(doc, 'r') as f:
-            	for line in f:
-        			values = line.split()
-        			word = values[0]
-        			vector = np.asarray(values[1:], "float32")
-        			self.embeddings_dict[word] = vector
-        			c += 1
-                    if c >= max_length_dictionary:
-                    	break
+		for doc in glove_files:
+			if j >= max_length_dictionary:
+				break
+			with open(doc, 'r') as file:
+				for line in file:
+					values = line.split()
+					word = values[0]
+					vector = np.asarray(values[1:], "float32")
+					self.embeddings_dict[word] = vector
+					j += 1
+					if j >= max_length_dictionary:
+						break						
 
 
 	#tweet = "Great to have a record 50 patrons"
 
-	def clean_text(x):
-    	tweet = x
-    	tweet = tweet.lower()
-    	#Removing numbers
-    	tweet = re.sub("^\d+\s|\s\d+\s|\s\d+$", " ", tweet)
-    	#Removing symbols
-    	tweet = re.sub(r'[^\w]', ' ', tweet)
-    	#Removing links
-    	tweet = re.sub(r"(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)", '', tweet)
+	@staticmethod
+	def remove_stop_word(tweet):
+        
 
-    	#Removing stopwords
-    	stop_words = set(stopwords.words('english')) 
+        stopwords = []
+        with open("english") as files:
+            for line in files:
+                values = line.split()
+                word = values[0]
+                stopwords.append(word)
+        pattern = re.compile(r'\b(' + r'|'.join(stopwords) + r')\b\s*')
+        tweet = pattern.sub('', tweet)
+        return tweet
 
-    	# import TweetTokenizer() method from nltk 
-    	from nltk.tokenize import TweetTokenizer 
+    def clean_text(self, tweet):
+        """
+        Clean text
+        """
 
-    	# Create a reference variable for Class TweetTokenizer 
-    	tk = TweetTokenizer() 
+        # URL
+        tweet = re.sub(r"(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)", '', tweet)
 
-    	# Use tokenize method 
-    	token = tk.tokenize(tweet)
+        tweet = tweet.lower()
 
-    	filtered_sentence = [w for w in token if not w in stop_words] 
+        # Numbers
+        tweet = re.sub(r"[0-9]+", '', tweet)
 
-    	filtered_sentence = [] 
+        # Stopwords
+        tweet = self.remove_sw(tweet)
 
-    	for w in token: 
-        	if w not in stop_words: 
-            	filtered_sentence.append(w) 
-    	s = " "
-    	s = s.join(filtered_sentence)
-    	return s
+        # Removing #
+        tweet = re.sub(r"#", '', tweet)
 
-    def tokenize_text(filtered_sentence):
-    	# import TweetTokenizer() method from nltk 
-    	from nltk.tokenize import TweetTokenizer 
+        # Removing handle
+        tweet = re.sub(r"@[a-zA-Z0-9]+", '', tweet)
 
-    	# Create a reference variable for Class TweetTokenizer 
-    	tk = TweetTokenizer() 
+        return tweet
 
-    	# Use tokenize method 
-    	token = tk.tokenize(filtered_sentence)
-    	return token
+    @staticmethod
+    def tokenize_text(tweet):
 
-    def replace_token_with_index(token):
-    	list_ = []
-    	for i in token:
-        	tuy = embeddings_dict.get(i, 0)
-        	if isinstance(tuy, np.ndarray):
-            	list_.append(tuy)
-    	return list_
+        """
+        Tokenize
+        """
+        tokenizer = TweetTokenizer()
+        return tokenizer.tokenize(tweet)
+
+    def replace_token_with_index(self, tweet):
+        """
+        Replace token
+        """
+        embd = []
+        for i in tweet:
+            index = self.embeddings_dict.get(i, 0)
+            if isinstance(index, np.ndarray):
+                embd.append(index)
+        return embd
 
     def pad_sequence(self, token_ind):
-        l = len(token_ind)
-        if l < max_length_tweet:
-            req_d = max_length_tweet - l
-            token_ind.extend([np.zeros_like(token_ind[0])]*req_d)
-        elif l > max_length_tweet:
-            token_ind = token_ind[:max_length_tweet].copy()
-            
+        """
+        Pad tokenized sequence
+        """
+        length = len(token_ind)
+        if length < self.max_length_tweet:
+            req_d = self.max_length_tweet - length
+            token_ind.extend([np.zeros_like(token_ind[0])] * req_d)
+        elif length > self.max_length_tweet:
+            token_ind = token_ind[:self.max_length_tweet].copy()
+
         return token_ind
 
-    
+
  #    cleaned_text = clean_text(tweet)
-	
+
 
 	# token = tokenize_text(cleaned_text)
-	
+
 	# token_ind = replace_token_with_index(token)
 
 	# token_ind_pad = pad_sequence(token_ind)
-
-
-
-
-
-
 
